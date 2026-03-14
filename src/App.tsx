d40037ef57fee721571cfb16ad2bda7ac5965402
Brayan <mrx.source.mrx@gmail.com>
2026-03-13 19:54:01 -0500

    feat: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { Navbar } from "./components/Navbar"
-import { NotestHeader } from "./components/NotesHeader"
+import { Notes } from "./Notes"
+import { useTheme } from "@/components/theme-provider"
 
 export function App() {
-	return (
-		<div className="flex min-h-svh justify-center">
-			{/* <div className="flex w-[50%] flex-col gap-4 bg-gray-800 text-sm leading-loose"> */}
-			<div className="flex w-[50%] flex-col gap-4 text-sm leading-loose">
-				<Navbar />
-				<NotestHeader />
-			</div>
-		</div>
-	)
+  const { setTheme, theme } = useTheme()
+
+  const toggleTheme = () => {
+    const nextTheme =
+      theme === "dark" ? "light" : theme === "light" ? "dark" : "dark"
+    setTheme(nextTheme)
+  }
+
+  return (
+    <div className="flex min-h-svh justify-center">
+      <div className="flex w-[50%] flex-col gap-4 text-sm leading-loose">
+        <Navbar theme={theme} toggleTheme={toggleTheme} />
+        <Notes theme={theme} />
+      </div>
+    </div>
+  )
 }
 
 export default App
